32fd6c50b864b49498c7eb7f3e2f4f9b46f1d98c
nevermore <vvs.frosti@gmail.com>
2025-09-11 09:46:21 +0200

    add gold apple

--- code/food.py
+++ code/food.py
@@ -2,26 +2,40 @@ import pygame
 import random
 from settings import ROWS, COLS, CELL
 
-class Food:
+class RandomFood:
     def __init__(self):
         self.random_position()
 
-    def random_position(self): #forbidden=None):
+    def random_position(self):
 
-        #if forbidden is None:
-        #    forbidden = set()
-
-        #while True:
         col = random.randint(0, COLS - 1)
         row = random.randint(0, ROWS - 1)
         self.position = (col, row)
 
-            # if (col, row) not in forbidden:
-            #     self.position = (col, row)
-            #     break
+    def random_food_number(self):
+        random_apple = random.randint(1, 11)
+
+        if random_apple != 1:
+            return 1
+        
+        else : return 0
+
+class Food(RandomFood):
+    def __init__(self):
+        super().__init__()
 
     def draw(self, surface):
         col, row = self.position
         x = col * CELL
         y = row * CELL
-        pygame.draw.rect(surface, (255, 0, 0), (x, y, CELL, CELL))
+        pygame.draw.rect(surface, (255, 0, 0), (x, y, CELL, CELL)) 
+
+class GoldApple(RandomFood):
+    def __init__(self):
+        super().__init__()
+
+    def draw(self, surface):
+        col, row = self.position
+        x = col * CELL
+        y = row * CELL
+        pygame.draw.rect(surface, (255, 255, 0), (x, y, CELL, CELL))
--- code/game.py
+++ code/game.py
@@ -1,7 +1,8 @@
 import pygame
 from settings import WIDTH, HEIGHT, TITLE, FPS, MOVE_EVENT, MOVES_PER_SEC
 from snake import DrawSnake
-from food import Food
+from food import Food, GoldApple, RandomFood
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -10,6 +11,20 @@ class Game:
         self.clock = pygame.time.Clock()
         self.snake = DrawSnake()
         self.food = Food()
+        self.gold_food = GoldApple()
+        self.random_food = RandomFood()
+
+        self.current_food = None
+        self.is_gold = False
+        self.spawn_food()
+
+    def spawn_food(self):
+        if self.random_food.random_food_number() == 1:
+            self.current_food = Food()
+            self.is_gold = False
+        else:
+            self.current_food = GoldApple()
+            self.is_gold = True
 
     def run(self):
 
@@ -18,21 +33,27 @@ class Game:
         pygame.time.set_timer(MOVE_EVENT, 1000 // MOVES_PER_SEC)
 
         while runs:
+
             for e in pygame.event.get():
                 if e.type == pygame.QUIT:
                     runs = False
                 elif e.type == MOVE_EVENT:
                     self.snake.step()
-                    if self.snake.head == self.food.position:
-                        self.food.random_position()
+
+                    if self.snake.head == self.current_food.position:
+                        self.spawn_food()
                         
                 elif e.type == pygame.KEYDOWN:
                     self.snake.change_dir(e.key)
-
+                    
             self.window.fill((144, 255, 100))
             self.clock.tick(FPS)
             self.snake.draw_snake(self.window)
-            self.food.draw(self.window)
+
+            if self.is_gold:
+                self.current_food.draw(self.window)
+            else:
+                self.current_food.draw(self.window)
 
             pygame.display.flip()
 
